200e614207d0dff434cf3373cde7ad1d0a1aa5e7
JradJR <86295986+JradJR@users.noreply.github.com>
2021-06-22 19:06:35 +0400

    Update main.js

--- main.js
+++ main.js
@@ -3,67 +3,67 @@ document.querySelector('header nav .fa').onclick = function() {
     this.classList.toggle('clicked')
 }
 
-// let links = document.querySelectorAll('header nav .links a');
-// links.forEach(link => {
-//     link.onclick = () => {
-//     document.querySelector('header nav .links').classList.remove('visible');
-//     document.querySelector('header nav .fa').classList.remove('clicked');
-//     }
-// })
+let links = document.querySelectorAll('header nav .links a');
+links.forEach(link => {
+    link.onclick = () => {
+    document.querySelector('header nav .links').classList.remove('visible');
+    document.querySelector('header nav .fa').classList.remove('clicked');
+    }
+})
 
-// // stll need to learn code  ((--  it's just copied  --))
+// stll need to learn code  ((--  it's just copied  --))
 
 
-// var scroll = window.requestAnimationFrame ||
-//              // IE Fallback
-//              function(callback){ window.setTimeout(callback, 1000/60)};
-// var elementsToShow = document.querySelectorAll('.about .skills .skill'); 
-// let portfolioItems = document.querySelectorAll('.portfolio .box, .about .box')
-// function loop() {
+var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.about .skills .skill'); 
+let portfolioItems = document.querySelectorAll('.portfolio .box, .about .box')
+function loop() {
 
-//   elementsToShow.forEach(function (element) {
-//     if (isElementInViewport(element)) {
-//       element.classList.add('visible');
-//     }else element.classList.remove('visible');
-//   });
+  elementsToShow.forEach(function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('visible');
+    }else element.classList.remove('visible');
+  });
 
-//   portfolioItems.forEach(item => {
-//     if(isElementInViewport(item)) item.classList.add('visible');
-//     else item.classList.remove('visible');
-//   })
+  portfolioItems.forEach(item => {
+    if(isElementInViewport(item)) item.classList.add('visible');
+    else item.classList.remove('visible');
+  })
   
-//     if(isElementInViewport(document.querySelector('.landing h2'))) {
-//         document.querySelector('.landing h2').classList.add('visible');
-//     }else {
-//         document.querySelector('.landing h2').classList.remove('visible');
-//     }
+    if(isElementInViewport(document.querySelector('.landing h2'))) {
+        document.querySelector('.landing h2').classList.add('visible');
+    }else {
+        document.querySelector('.landing h2').classList.remove('visible');
+    }
 
 
-//     if(isElementInViewport(document.querySelector('.contact .info'))) {
-//         document.querySelector('.contact .info').classList.add('visible');
-//     }else {
-//         document.querySelector('.contact .info').classList.remove('visible');
-//     }
-//   scroll(loop);
-// }
+    if(isElementInViewport(document.querySelector('.contact .info'))) {
+        document.querySelector('.contact .info').classList.add('visible');
+    }else {
+        document.querySelector('.contact .info').classList.remove('visible');
+    }
+  scroll(loop);
+}
 
-// // Call the loop for the first time
-// loop();
+// Call the loop for the first time
+loop();
 
-// // Helper function from: http://stackoverflow.com/a/7557433/274826
-// function isElementInViewport(el) {
-//   // special bonus for those using jQuery
-//   var rect = el.getBoundingClientRect();
-//   return (
-//     (rect.top <= 0
-//       && rect.bottom >= 0)
-//     ||
-//     (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-//       rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-//     ||
-//     (rect.top >= 0 &&
-//       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-//   );
-// }
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
 
 // stll need to learn code 
