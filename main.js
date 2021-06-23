5f390e31394d40c53d89f0ed8e489b6ae2b4d48b
JradJR <rahmyr1979@gmail.com>
2021-06-23 04:10:58 +0400

    update the main.js with better code

--- main.js
+++ main.js
@@ -1,74 +1,46 @@
-document.querySelector('header nav .fa').onclick = function() {
-    document.querySelector('header nav .links').classList.toggle('visible');
+let menue = document.querySelector('header nav .fa');
+let links = document.querySelector('header nav .links');
+
+menue.onclick = function() {
+    links.classList.toggle('visible');
     this.classList.toggle('clicked')
 }
 
-let links = document.querySelectorAll('header nav .links a');
-links.forEach(link => {
+let linksItems = document.querySelectorAll('header nav .links a');
+
+linksItems.forEach(link => {
     link.onclick = () => {
-    document.querySelector('header nav .links').classList.remove('visible');
-    document.querySelector('header nav .fa').classList.remove('clicked');
+    links.classList.remove('visible');
+    menue.classList.remove('clicked');
     }
+});
+
+
+
+let mainHeading = document.querySelector('.landing h2');
+let portfolioBoxes = document.querySelectorAll('.portfolio .box');
+let aboutItems = document.querySelectorAll('.about .box');
+let contactBox = document.querySelector('.contact .info');
+
+const observer = new IntersectionObserver(items => {
+
+    items.forEach(item => {
+        if(item.isIntersecting) {
+            item.target.classList.add('visible');
+            return;
+        }
+        item.target.classList.remove('visible');
+    })
 })
 
-// stll need to learn code  ((--  it's just copied  --))
+observer.observe(mainHeading);
 
+portfolioBoxes.forEach(box => {
+    observer.observe(box);
+})
 
-let scroll = window.requestAnimationFrame ||
-               // IE Fallback
-         function(callback){ window.setTimeout(callback, 1000/60)};
-let elementsToShow = document.querySelectorAll('.about .skills .skill'); 
-let portfolioItems = document.querySelectorAll('.portfolio .box, .about .box')
-let header = document.querySelector('header');
-function loop() {
-    window.setTimeout(() => {
-        elementsToShow.forEach(function (element) {
-        if (isElementInViewport(element)) {
-            element.classList.add('visible');
-        }else if(isElementInViewport(header)) element.classList.remove('visible');
-        });
-    
-        portfolioItems.forEach(item => {
-        if(isElementInViewport(item)) item.classList.add('visible');
-        else if(isElementInViewport(header)) item.classList.remove('visible');
-        })
-        
-        if(isElementInViewport(document.querySelector('.landing h2'))) {
-            document.querySelector('.landing h2').classList.add('visible');
-        }else if(isElementInViewport(header)) {
-            document.querySelector('.landing h2').classList.remove('visible');
-        }
-    
-    
-        if(isElementInViewport(document.querySelector('.contact .info'))) {
-            document.querySelector('.contact .info').classList.add('visible');
-        }else  if(isElementInViewport(header)) {
-            document.querySelector('.contact .info').classList.remove('visible');
-        }
-        console.log('looped');
-        scroll(loop);
-    }, 10)
-}
+aboutItems.forEach(item => {
+    observer.observe(item);
+})
 
-// Call the loop for the first time
-window.setTimeout(loop, 500)
-
-
-
-// Helper function from: http://stackoverflow.com/a/7557433/274826
-function isElementInViewport(el) {
-  // special bonus for those using jQuery
-  var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
-}
-
-// stll need to learn code 
+observer.observe(contactBox);
